cf37786b5a8228d6b8d82a5da9028e2dd7ea5b0b
Sebastian Brunner <brunner.sebi4@gmail.com>
2025-05-09 17:23:30 +0200

    rename threesat

--- g4satbench/data/usat.py
+++ g4satbench/data/usat.py
@@ -1,15 +1,12 @@
-import copy
 import random
 
-import numpy as np
-import networkx as nx
 from pysat.solvers import Cadical195 as Cadical
 from torch.utils.data import IterableDataset
-from g4satbench.utils.utils import VIG, clean_clauses, hash_clauses
+
 from g4satbench.data.data import construct_lcg
 
 
-class ThreeSatDataset(IterableDataset):
+class USatDataset(IterableDataset):
     def __init__(self, nr_gen_instances, min_n, max_n, min_m, max_m, min_l, max_l):
         self.nr_gen_instances = nr_gen_instances
         self.min_n = min_n
@@ -21,7 +18,7 @@ class ThreeSatDataset(IterableDataset):
         self.hash_list = []
 
     def __len__(self):
-        return 2 * self.nr_gen_instances
+        return self.nr_gen_instances
 
     def __iter__(self):
         for i in range(self.nr_gen_instances):
@@ -51,7 +48,7 @@ class ThreeSatDataset(IterableDataset):
 
 
 if __name__ == '__main__':
-    dataset = ThreeSatDataset(1000, 4, 8, 15, 25, 2, 5)
+    dataset = USatDataset(1000, 4, 8, 15, 25, 2, 5)
     sat = 0
     unsat = 0
     for data in dataset:
